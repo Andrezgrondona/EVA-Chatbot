c1d6cce9b9fd4b750bdb4499bd3d6122a607621e
andresgrondona <afgo4ster@gmail.com>
2025-04-10 11:53:23 -0500

    fix err

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 // index.js
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const chatRoute = require('./routes/chat');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/chat', chatRoute);
-
 app.get('/', (req, res) => {
-  res.send('Eva está en línea 🧠✨');
+  res.send('Hola desde EVA!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-
-
-//node index.js
