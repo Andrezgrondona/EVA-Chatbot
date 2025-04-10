d79e58b68e11ab780679f411d992b1708ca89364
andresgrondona <afgo4ster@gmail.com>
2025-04-10 11:59:36 -0500

    fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// index.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,5 +12,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
